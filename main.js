7f5ed57ea9e1e6f522ebb53081991aecb99cbeb9
edwarddubi <59847429+edwarddubi@users.noreply.github.com>
2020-11-27 18:06:17 -0500

    fixed player equal turns bug

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ client.once("ready", () => {
 let players = [];
 let temp_players = players.slice(0);
 let maxPlayers = 0;
-let maxPlays = 5;
+let maxPlays = 3;
+let round_num = 1;
 let awake = false;
 let done = false;
 client.on("message", (message) => {
@@ -40,6 +41,10 @@ client.on("message", (message) => {
         if (!found) {
           players.push(currentPlayer);
           message.channel.send(`${name} joined the game!`);
+        } else {
+          message.channel.send(
+            `${name}, hold up! You cannot join a game you're already part of!`
+          );
         }
       }
       return;
@@ -50,19 +55,21 @@ client.on("message", (message) => {
         team += c + ". " + player["username"] + "\n";
         c++;
       });
-      team += "Now let the game begin!!";
+      team += "This is the beginning of round 1:";
       message.channel.send(team);
       temp_players = players.slice(0);
       done = true;
       return;
-    }else if (done && message.content.toLowerCase().startsWith("rolldice")) {
-        console.log(maxPlays)
+    } else if (done && message.content.toLowerCase().startsWith("rolldice")) {
+      if (temp_players.length === 0) {
+        temp_players = players.slice(0);
+        maxPlays -= 1;
         if (maxPlays === 0) {
-          
-          let team = "Who got the RNG trophy?:\n";
+          let team = "Let's see who got the Crown:\n";
           let c = 1;
           players.forEach((player) => {
-            team += c + ". " + player["username"] + ": " + player["points"] + "\n";
+            team +=
+              c + ". " + player["username"] + ": " + player["points"] + "\n";
             c++;
           });
           const winner_player = winner(players);
@@ -76,55 +83,48 @@ client.on("message", (message) => {
           temp_players = players.slice(0);
           return;
         } else {
-          if (temp_players.length === 0) {
-            temp_players = players.slice(0);
-            maxPlays -= 1;
-          }
-         
-          const id = message.author.id;
-          let found = false;
-          let c = 0;
-          temp_players.forEach((player) => {
-            if (player["id"] === id) {
-              found = true;
-              temp_players.splice(c, 1);
-              
-            }
-            c++;
-          });
-          console.log(temp_players.length)
-          if (found) {
-            players.forEach((player) => {
-              if (player["id"] === id) {
-                const rand = Math.floor(Math.random() * 6) + 1;
-                message.channel.send(`${player["username"]}, rolled a ${rand}`)
-                player["points"] = player["points"] + rand;
-              }
-            });
-          } else {
-            message.channel.send(
-              "You need to wait for your turn in the next Round."
-            );
-          }
+          message.channel.send(`This is the next Round: `);
         }
-        return;
       }
-      
-  }  else {
+      console.log("rounds left:", maxPlays);
+      console.log("total players:", temp_players.length);
+      const id = message.author.id;
+      let found = false;
+      let c = 0;
+      temp_players.forEach((player) => {
+        if (player["id"] === id) {
+          found = true;
+          temp_players.splice(c, 1);
+        }
+        c++;
+      });
+
+      if (found) {
+        players.forEach((player) => {
+          if (player["id"] === id) {
+            const rand = Math.floor(Math.random() * 6) + 1;
+            message.channel.send(`${player["username"]}, rolled a ${rand}`);
+            player["points"] = player["points"] + rand;
+          }
+        });
+      } else {
+        message.channel.send(
+          "You need to wait for your turn in the next Round."
+        );
+      }
+    }
+  } else {
     if (
       message.content.toLowerCase().startsWith(initPlay.toLowerCase()) &&
       !message.author.bot
     ) {
       message.channel.send(
-        "Thanks for waking me up from my slumber. Rules for the game are:\n" +
-          "1. Players should type play to be added to game\n2. Anyone type done\n3. type rollDice\n4. you will roll a random number between 1-6\n5. Total number rolled during the game will determine the winner\n6. Play away!! test how good your RNG is...hahahaha"
+        `Thanks for waking me up from my slumber.\nRules for the game are:\n1. Players should type "play" to be added to game.\n2. Anyone can type "done" when all participating players have joined.\n3. Now, to start playing, type "rollDice".\n4. You will roll a number between 1-6.\n5. Rounds to be played (3), so players would take turns until each round has ended.\n6. Total number rolled during the game will determine the winner\n6. Play away!! And test how good your RNG is...hahahaha`
       );
       awake = true;
       return;
     }
   }
-
-  
 
   //const args = message.content.slice(initPlay.length).split("#");
 
@@ -135,7 +135,7 @@ const winner = (players) => {
   let player = players[0];
   players.forEach((element) => {
     if (player["points"] < element["points"]) {
-        player = element;
+      player = element;
     }
   });
 
